8874f87ef56ef158c682d36753c1343704c8ae7b
Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>
2020-07-31 15:28:00 +0530

    Adding Route in publisher rating
    -getting all publisher with average ratings
    
    Signed-off-by: Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>

--- controllers/publisherrating.js
+++ controllers/publisherrating.js
@@ -1,4 +1,5 @@
 const Publisherrating = require("../models/publisherRating");
+const Publisher = require("../models/publisher");
 
 exports.getAllPublisherRatings = async (req, res, next) => {
   try {
@@ -42,6 +43,65 @@ exports.getRatingForPublisherId = async (req, res, next) => {
     });
     res.status(200).json({ success: true, data: rating });
   } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+};
+
+exports.getAllPublishersWithAverageRating = async (req, res, next) => {
+  try {
+    let allPublishersWithAvgRating = await Publisher.aggregate([
+      {
+        $lookup: {
+          from: Publisherrating.collection.name,
+          localField: "_id",
+          foreignField: "publisher",
+          as: "pubrating",
+        },
+      },
+      { $unwind: { path: "$pubrating", preserveNullAndEmptyArrays: true } },
+      {
+        $group: {
+          _id: "$_id",
+          name: { $first: "$name" },
+          verified: { $first: "$verified" },
+          email: { $first: "$email" },
+          about: { $first: "$about" },
+          website: { $first: "$website" },
+          address: { $first: "$address" },
+          logo: { $first: "$logo" },
+          userId: { $first: "$userId" },
+          city: { $first: "$city" },
+          zip: { $first: "$zip" },
+          urlStr: { $first: "$urlStr" },
+          count: { $sum: 1 },
+          totalRating: { $sum: "$pubrating.value" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          name: 1,
+          email: 1,
+          about: 1,
+          website: 1,
+          address: 1,
+          logo: 1,
+          userId: 1,
+          verified: 1,
+          urlStr: 1,
+          id: "$_id",
+          averageRating: {
+            $round: [{ $divide: ["$totalRating", "$count"] }, 1],
+          },
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      data: allPublishersWithAvgRating,
+    });
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ success: false, error });
   }
 };
--- routes/publisher-rating-route.js
+++ routes/publisher-rating-route.js
@@ -4,6 +4,7 @@ const checkAuth = require("../middleware/check-auth");
 /**controller function for publisher rating */
 const {
   getAllPublisherRatings,
+  getAllPublishersWithAverageRating,
   savePublisherRating,
   getRatingForPublisherId,
 } = require("../controllers/publisherrating");
@@ -14,21 +15,28 @@ const {
 } = require("./validation/publisherrating");
 
 /**
- * @description   this route is used to get all preferences
+ * @description   this route is used to get all publisher ratings
  * @route   GET      /api/publisherrating
  * @access  Public
  */
 router.get("/", getAllPublisherRatings);
 
 /**
- * @description   this route is used to get all preferences
+ * @description   this route is used to get all publishers with average ratings
+ * @route   GET      /api/publisherrating/allpublisher
+ * @access  Public
+ */
+router.get("/allpublisher", getAllPublishersWithAverageRating);
+
+/**
+ * @description   this route is used to get all publisher ratings
  * @route   POST      /api/publisherrating
  * @access  Private
  */
 router.post("/", checkAuth, validateOnSavePublisherRating, savePublisherRating);
 
 /**
- * @description   this route is used to get all preferences
+ * @description   this route is used to get all publisher ratings
  * @param - publisherId
  * @route   GET      /api/publisherrating/:user/:publisherId
  * @access  Private

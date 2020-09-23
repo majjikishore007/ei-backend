4eac64728dbb0f1d7ada675a2571d256bd1389f9
Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>
2020-09-23 12:50:14 +0530

    publisher page bug fix
    Signed-off-by: Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>

--- routes/validation/publisher.js
+++ routes/validation/publisher.js
@@ -4,39 +4,39 @@ exports.validateCreatePublisher = async (req, res, next) => {
   try {
     if (!req.body.name) {
       return res
-        .status(400)
+        .status(200)
         .json({ success: false, message: "Please enter valid name" });
     }
     if (!req.body.email) {
       return res
-        .status(400)
+        .status(200)
         .json({ success: false, message: "Please enter valid email" });
     }
     if (!req.body.about) {
       return res
-        .status(400)
+        .status(200)
         .json({ success: false, message: "Please enter valid about" });
     }
     if (!req.body.website) {
       return res
-        .status(400)
+        .status(200)
         .json({ success: false, message: "Please enter valid website" });
     }
     if (!req.body.address) {
       return res
-        .status(400)
+        .status(200)
         .json({ success: false, message: "Please enter valid address" });
     }
     let emailExist = await publisher.findOne({ email: req.body.email });
     if (emailExist) {
       return res
-        .status(400)
+        .status(200)
         .json({ success: false, message: "Email already exist" });
     }
     let websiteExist = await publisher.findOne({ website: req.body.website });
     if (websiteExist) {
       return res
-        .status(400)
+        .status(200)
         .json({ success: false, message: "Website already exist" });
     }
     next();
